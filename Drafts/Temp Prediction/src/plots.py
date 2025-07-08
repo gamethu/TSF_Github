4b573024cf5c71fd19166913393f322dad703205
NguyenTanDai_3122411035 <70790046+gamethu@users.noreply.github.com>
2025-07-08 20:04:17 +0700

    hoan thien 1/4 model RF

--- Drafts/Temp Prediction/src/plots.py
+++ Drafts/Temp Prediction/src/plots.py
@@ -57,7 +57,7 @@ def plot_Outlier(data, data_cols, target=None):
     plt.show()
 
 # Mutual infomation
-def make_mi_scores(X_data, y_data):
+def make_mi_scores_classification(X_data, y_data):
     import pandas as pd
     from sklearn.feature_selection import mutual_info_classif
     
@@ -68,6 +68,20 @@ def make_mi_scores(X_data, y_data):
     # All discrete features should now have integer dtypes
     discrete_features = [pd.api.types.is_integer_dtype(t) for t in X_data.dtypes]
     mi_scores = mutual_info_classif(X_data, y_data, discrete_features=discrete_features, random_state=0)
+    mi_scores = pd.Series(mi_scores, name="MI Scores", index=X_data.columns)
+    mi_scores = mi_scores.sort_values(ascending=False)
+    return mi_scores
+def make_mi_scores_regression(X_data, y_data):
+    import pandas as pd
+    from sklearn.feature_selection import mutual_info_regression
+    
+    X_data = X_data.copy()
+    for colname in X_data.select_dtypes(["object", "category"]):
+        X_data[colname], _ = X_data[colname].factorize()
+
+    # All discrete features should now have integer dtypes
+    discrete_features = [pd.api.types.is_integer_dtype(t) for t in X_data.dtypes]
+    mi_scores = mutual_info_regression(X_data, y_data, discrete_features=discrete_features, random_state=0)
     mi_scores = pd.Series(mi_scores, name="MI Scores", index=X_data.columns)
     mi_scores = mi_scores.sort_values(ascending=False)
     return mi_scores
